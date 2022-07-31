2633bb78108dad011d5bfdd6181e399b0afe8f67
Fids2323 <workfids23@gmail.com>
2022-07-31 11:00:10 +0300

    modal&check

--- js/app.js
+++ js/app.js
@@ -15,13 +15,49 @@ const technologies = [
 //add Eventlistener to modalWindow
 content.addEventListener('click', openCard);
 backdrop.addEventListener('click', closeModal);
+modal.addEventListener('change', toggleTech)
 
-//create function
+const APP_TITLE = document.title;
+
+//modal
 function openCard() {
+	const data = event.target.dataset
+	//find(search) array iteration, comparison.doesnt't open modal outside the cards
+	const tech = technologies.find(t => t.type === data.type)
+	if (!tech) return
+
+	openModal(toModal(tech), tech.title)
+}
+
+function toModal(tech) {
+	const checked = tech.done ? 'checked' : ''
+	return `
+	<h2>${tech.title}</h2>
+			<p>${tech.description}</p>
+			<hr>
+			<div>
+				<input type="checkbox" id="done" ${checked} data-type="${tech.type}" >
+				<label for="done">Выучил</label>
+			</div>
+	`
+}
+//check
+function toggleTech(event) {
+	const type = event.target.dataset.type
+	const tech = technologies.find(t => t.type === type)
+	tech.done = event.target.checked
+
+	init()
+}
+
+function openModal(html, title = APP_TITLE) {
+	document.title = `${title} | ${APP_TITLE}`
+	modal.innerHTML = html
 	modal.classList.add('open');
 }
 
 function closeModal() {
+	document.title = APP_TITLE
 	modal.classList.remove('open');
 }
 
@@ -91,9 +127,10 @@ function toCard(tech) {
 
 	const doneClass = tech.done ? 'done' : ''
 
+	// data-type-custom atr(for click cards and text in cards)
 	return `
-	<div class="card ${doneClass}">
-		<h3>${tech.title}</h3>
+	<div class="card ${doneClass}" data-type="${tech.type}">
+		<h3 data-type="${tech.type}">${tech.title}</h3>
 	</div>
 	`
 }
